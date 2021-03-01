2459dfd08293cff6b8a5b79b043df0f0dc8a2b5b
Dan Worlton <worltondanielp@johndeere.com>
2021-03-01 15:36:31 -0600

    Add initial tests for 3 and pass and test for 5

---
+++ src/main/java/com/csacademy/FizzBuzz.java
@@ -0,0 +1,10 @@
+package com.csacademy;
+
+public class FizzBuzz {
+    public static String calc(int i) {
+        if(i == 3) {
+            return "Fizz";
+        }
+        return null;
+    }
+}
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,10 +1,29 @@
+import com.csacademy.FizzBuzz;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class FizzBuzzTest {
     @Test
-    public void myFirstTest() {
+    public void shouldReturnFizzFor3() {
+        //Arrange or Given
+        int givenInput = 3;
 
+        //Act or When
+        String returnVal = FizzBuzz.calc(givenInput);
+
+        //Assert or Then
+        assertEquals("Fizz", returnVal);
+    }
+    @Test
+    public void shouldReturnBuzzFor5() {
+        //Arrange or Given
+        int givenInput = 5;
+
+        //Act or When
+        String returnVal = FizzBuzz.calc(givenInput);
+
+        //Assert or Then
+        assertEquals("Buzz", returnVal);
     }
 }
